c5a38c3dd9efd7d8743eb5e31330e26e4ee4b505
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-12-22 18:45:03 -0300

    Stage 1: Refactor command classes into separate file
    The command classes (SetCommand, GetCommand, DeleteCommand) and CommandProcessor have been moved from Main.kt to a new dedicated file, Commands.kt. This improves the structure of the code by grouping related classes together in a separate file.

--- JSON Database (Kotlin)/task/src/jsondatabase/server/Main.kt
+++ JSON Database (Kotlin)/task/src/jsondatabase/server/Main.kt
@@ -25,42 +25,6 @@ class TextDatabase {
         } else false
 }
 
-interface Command {
-    fun execute(): String
-}
-
-class SetCommand(private val db: TextDatabase, private val index: Int, private val text: String) : Command {
-    override fun execute(): String =
-        if (db.set(index, text)) SUCCESS_RES else ERROR_RES
-}
-
-class GetCommand(private val db: TextDatabase, private val index: Int) : Command {
-    override fun execute(): String =
-        db.get(index) ?: ERROR_RES
-}
-
-class DeleteCommand(private val db: TextDatabase, private val index: Int) : Command {
-    override fun execute(): String =
-        if (db.delete(index)) SUCCESS_RES else ERROR_RES
-}
-
-class CommandProcessor(private val db: TextDatabase) {
-    private val commands = mapOf(
-        "set" to { index: Int, args: List<String> -> SetCommand(db, index, args.joinToString(" ")) },
-        "get" to { index: Int, _: List<String> -> GetCommand(db, index) },
-        "delete" to { index: Int, _: List<String> -> DeleteCommand(db, index) }
-    )
-
-    fun getCommand(command: List<String>?): Command {
-        val cmd = command?.getOrNull(0)
-        val index = command?.getOrNull(1)?.toIntOrNull() ?: throw IllegalArgumentException("Invalid index")
-        val args = command.drop(2)
-        val commandFunc = commands[cmd] ?: throw IllegalArgumentException("Unknown command")
-        return commandFunc(index, args)
-    }
-}
-
-
 fun main() {
     val db = TextDatabase()
     val processor = CommandProcessor(db)
